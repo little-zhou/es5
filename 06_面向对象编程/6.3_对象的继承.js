1d6be2eeb8e90f44eccd1289114c2da6c52b1ef1
little-zhou <424048725@qq.com>
2024-04-24 22:45:00 +0800

    异步操作

--- 06_面向对象编程/6.3_对象的继承.js
+++ 06_面向对象编程/6.3_对象的继承.js
@@ -40,10 +40,105 @@
 // console.log(mine.length); // 3
 // console.log(mine instanceof Array); // true
 
-function Foo() {}
-console.log(Foo.prototype.constructor === Foo); // true
-var foo = new Foo();
-console.log(foo.constructor == Foo); // true
-console.log(foo.constructor == Foo.prototype.constructor); // true
-console.log(foo.hasOwnProperty('constructor')); // false
+// function Foo() {}
+// console.log(Foo.prototype.constructor === Foo); // true
+// var foo = new Foo();
+// console.log(foo.constructor == Foo); // true
+// console.log(foo.constructor == Foo.prototype.constructor); // true
+// console.log(foo.hasOwnProperty('constructor')); // false
 
+// function Shape() {
+//   this.x = 0;
+//   this.y = 0;
+// }
+
+// Shape.prototype.move = function(x, y) {
+//   this.x += x;
+//   this.y += y;
+//   console.info('Shape moved.');
+// };
+
+// // 第一步,子类继承父类的实例
+// function Rectangle() {
+//     Shape.call(this); // 调用父类的构造函数
+// }
+
+// // 第二步,子类继承父类的原型
+// // 必须要用Object.create否则父类的原型Shape.prototype也会被改掉
+// Rectangle.prototype = Object.create(Shape.prototype);
+// Rectangle.prototype.constructor = Rectangle;
+
+// var rect = new Rectangle();
+// console.log(rect instanceof Shape); // true
+// console.log(rect instanceof Rectangle); // true
+
+// ClassB.prototype.print = function() {
+//   ClassA.prototype.print.call(this);
+//   // some code
+// }
+
+
+// function M1() {
+//   this.hello = 'hello';
+//   this.sayHello = function() {
+//     console.log('---hello---');
+//   }
+// }
+
+// function M2() {
+//   this.world = 'world';
+//   this.sayWorld = function() {
+//     console.log('---world---');
+//   }
+// }
+
+// function S() {
+//   // 调用父类的构造函数
+//   M1.call(this);
+//   M2.call(this);
+// }
+
+// var s = new S();
+// console.log(s.hello); // hello 
+// console.log(s.world); // world
+
+// // 继承M1
+// S.prototype = Object.create(M1.prototype);
+// // 继承链上加入M2
+// Object.assign(S.prototype, M2.prototype);
+
+// // 指定构造函数
+// S.prototype.constructor = S;
+
+// var s1 = new S();
+// s1.sayHello();
+// s1.sayWorld();
+
+
+// var module = (function() {
+//   var _count = 0;
+//   var m1 = function() { return 1 };
+//   var m2 = function() {};
+//   return {
+//     m1: m1,
+//     m2: m2
+//   };
+// })();
+// // 读取不到内部的_count变量。
+// console.log(module._count); // undefined
+// console.log(module.m1()); // 1
+
+
+// var module = (function(mod) {
+//   mod.m3 = function() {
+//     console.log('m3');
+//   };
+//   return mod;
+// })(module);
+// module.m3(); // m3
+
+// var module = (function(mod) {
+//   // ...
+// })(window.module || {});
+
+console.log(undefined || {});
